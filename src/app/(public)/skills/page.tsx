1177d2c68b147f2496db5872048dc103ad5fc2c7
Suronjit797 <suronjit797@gmail.com>
2025-01-29 13:21:30 +0600

    skills semi findal

--- src/app/(public)/skills/page.tsx
+++ src/app/(public)/skills/page.tsx
@@ -1,16 +1,19 @@
-"use client";
-
-import { useState, useEffect } from "react";
 import SkillCard from "@/components/SkillCard";
 import { ISkillData } from "@/interfaces/skills";
-import Loading from "@/components/loading/Loading";
 
 const Skills: React.FC = () => {
-  const [frontend, setFrontend] = useState<ISkillData[]>([]);
-  const [backend, setBackend] = useState<ISkillData[]>([]);
-  const [data, setData] = useState<ISkillData[]>([]);
-  const [development, setDevelopment] = useState<ISkillData[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  // data
+
+  const frontend: ISkillData[] = [];
+  const backend: ISkillData[] = [];
+  const data: ISkillData[] = [];
+  const development: ISkillData[] = [];
+
+  // const [frontend, setFrontend] = useState<ISkillData[]>([]);
+  // const [backend, setBackend] = useState<ISkillData[]>([]);
+  // const [data, setData] = useState<ISkillData[]>([]);
+  // const [development, setDevelopment] = useState<ISkillData[]>([]);
+  // const [isLoading, setIsLoading] = useState<boolean>(true);
 
   //   const getFrontend = async () => {
   //     try {
@@ -48,82 +51,78 @@ const Skills: React.FC = () => {
   //     }
   //   };
 
-  const fetchData = async () => {
-    setIsLoading(true);
-    // await getFrontend();
-    // await getBackend();
-    // await getData();
-    // await getDevelopment();
-    setIsLoading(false);
-  };
+  // const fetchData = async () => {
+  //   setIsLoading(true);
+  //   // await getFrontend();
+  //   // await getBackend();
+  //   // await getData();
+  //   // await getDevelopment();
+  //   setIsLoading(false);
+  // };
 
-  useEffect(() => {
-    fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   return (
     <>
-      {isLoading ? (
-        <Loading isLoading={isLoading} />
-      ) : (
-        <div className="frontend px-lg-5 py-5 px-3 ">
-          <div>
-            <div className="mb-4">
-              <h3 className="fw-bold heading">
-                <span> Frontend </span> Skills
-              </h3>
-              <hr />
-            </div>
-            <div className="grid grid-rows-2 md:grid-rows-4 lg:grid-rows-6 gap-4">
-              {frontend.map((data) => (
-                <SkillCard key={data.name} data={data} />
-              ))}
-            </div>
+      <div className="frontend px-lg-5 py-5 px-3 ">
+        <div>
+          <div className="mb-4">
+            <h3 className="fw-bold heading">
+              <span> Frontend </span> Skills
+            </h3>
+            <hr />
           </div>
-          <div>
-            <div className="my-4 pt-4">
-              <h3 className="fw-bold heading">
-                <span> Backend </span> Skills
-              </h3>
-              <hr />
-            </div>
-            <div className="grid grid-rows-2 md:grid-rows-4 lg:grid-rows-6 gap-4">
-              {backend.map((data) => (
-                <SkillCard key={data.name} data={data} />
-              ))}
-            </div>
-          </div>
-          <div>
-            <div className="my-4 pt-4">
-              <h3 className="fw-bold heading">
-                <span> State & Data Management </span>
-                Skills
-              </h3>
-              <hr />
-            </div>
-            <div className="grid grid-rows-2 md:grid-rows-4 lg:grid-rows-6 gap-4">
-              {data.map((data) => (
-                <SkillCard key={data.name} data={data} />
-              ))}
-            </div>
-          </div>
-          <div>
-            <div className="my-4 pt-4">
-              <h3 className="fw-bold heading">
-                <span> Development & Design Tools </span>
-                Skills
-              </h3>
-              <hr />
-            </div>
-            <div className="grid grid-rows-2 md:grid-rows-4 lg:grid-rows-6 gap-4">
-              {development.map((data) => (
-                <SkillCard key={data.name} data={data} />
-              ))}
-            </div>
+          <div className="grid grid-rows-2 md:grid-rows-4 lg:grid-rows-6 gap-4">
+            {frontend.map((data) => (
+              <SkillCard key={data.name} data={data} />
+            ))}
           </div>
         </div>
-      )}
+        <div>
+          <div className="my-4 pt-4">
+            <h3 className="fw-bold heading">
+              <span> Backend </span> Skills
+            </h3>
+            <hr />
+          </div>
+          <div className="grid grid-rows-2 md:grid-rows-4 lg:grid-rows-6 gap-4">
+            {backend.map((data) => (
+              <SkillCard key={data.name} data={data} />
+            ))}
+          </div>
+        </div>
+        <div>
+          <div className="my-4 pt-4">
+            <h3 className="fw-bold heading">
+              <span> State & Data Management </span>
+              Skills
+            </h3>
+            <hr />
+          </div>
+          <div className="grid grid-rows-2 md:grid-rows-4 lg:grid-rows-6 gap-4">
+            {data.map((data) => (
+              <SkillCard key={data.name} data={data} />
+            ))}
+          </div>
+        </div>
+        <div>
+          <div className="my-4 pt-4">
+            <h3 className="fw-bold heading">
+              <span> Development & Design Tools </span>
+              Skills
+            </h3>
+            <hr />
+          </div>
+          <div className="grid grid-rows-2 md:grid-rows-4 lg:grid-rows-6 gap-4">
+            {development.map((data) => (
+              <SkillCard key={data.name} data={data} />
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
