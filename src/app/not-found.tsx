777c1f45c63969875493d6272e44dbdef89b7f25
Suronjit797 <suronjit797@gmail.com>
2025-02-10 14:12:55 +0600

    accessibility change

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,27 @@
+"use client";
+
+import { Button, Result } from "antd";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function NotFound() {
+  const router = useRouter();
+
   return (
     <>
-      <div> not found</div>
-      <Link href="/"> Back Home</Link>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <div>
+            <Link href="/">
+              <Button type="primary">Go to Home</Button>
+            </Link>
+            <Button variant="outlined" color="blue" className="ml-2"  onClick={()=>router.back()}>Go Back</Button>
+          </div>
+        }
+      />
     </>
   );
 }
--- src/components/header/Sidebar.tsx
+++ src/components/header/Sidebar.tsx
@@ -25,10 +25,10 @@ const Sidebar = () => {
   return (
     <div className="h-screen w-full max-w-screen py-3 px-3 overflow-y-auto overflow-x-hidden bg-[#212529]">
       <div className="flex items-center flex-col h-full">
-        <Image src={user} alt="user" className={`${styles.userImage}`} />
+        <Image src={user} height={200} width={200} alt="user" className={`${styles.userImage}`} />
         <div className="intro text-center mt-3 mb-2">
-          <h3 className="text-3xl text-primary underline mb-1"> Suronjit Pal </h3>
-          <h6> MERN Stack Developer </h6>
+        <h1 className="text-3xl text-primary underline mb-1"> Suronjit Pal </h1>
+        <h2 className="text-md"> MERN Stack Developer </h2>
         </div>
 
         {/* nav items */}
@@ -48,24 +48,39 @@ const Sidebar = () => {
         <hr className="w-full mt-auto" />
 
         {/* social items */}
-        <div className={`mt-3 flex justify-between`}>
+        <div className="mt-3 flex justify-between">
           <div className={`${styles.icon} ${styles.facebook}`}>
-            <a href="https://www.facebook.com/suronjit797/" target="_blank" className="!text-white">
+            <a
+              href="https://www.facebook.com/suronjit797/"
+              target="_blank"
+              aria-label="Facebook Profile"
+              className="!text-white"
+            >
               <FaFacebookF />
             </a>
           </div>
           <div className={`${styles.icon} ${styles.linkedin}`}>
-            <a href="https://www.linkedin.com/in/suronjit797/" target="_blank" className="!text-white">
+            <a
+              href="https://www.linkedin.com/in/suronjit797/"
+              target="_blank"
+              aria-label="LinkedIn Profile"
+              className="!text-white"
+            >
               <FaLinkedinIn />
             </a>
           </div>
           <div className={`${styles.icon} ${styles.gmail}`}>
-            <a href="mailto:suronjit797@gmail.com" target="_blank" className="!text-white">
+            <a href="mailto:suronjit797@gmail.com" target="_blank" aria-label="Send an Email" className="!text-white">
               <BiLogoGmail />
             </a>
           </div>
           <div className={`${styles.icon} ${styles.github}`}>
-            <a href="https://github.com/suronjit797" target="_blank" className="!text-white">
+            <a
+              href="https://github.com/suronjit797"
+              target="_blank"
+              aria-label="GitHub Profile"
+              className="!text-white"
+            >
               <FaGithub />
             </a>
           </div>
